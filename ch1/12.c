30997a61535ed720fbd610bad6fd80663e8d5e88
Joshua Licudo <joshua.licudo.753@my.csun.edu>
2023-02-07 14:03:49 -0800

    Ch.1 Ex.12

---
+++ ch1/12.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "kandr-01-11.h"
+
+#define IN 1
+#define OUT 0
+
+void word_split(FILE *stream) {
+    int c, state;
+
+    state = OUT;
+    while ((c = getc(stream)) != EOF) {
+        if (c == ' ' || c == '\n' || c == '\t') {
+            if (state == IN) {
+                putc('\n', stdout);
+            }
+            state = OUT;
+        } else {
+            if (state == OUT) {
+                state = IN;
+            }
+            putc(c, stdout);
+        }
+    }
+}
+
+int main() {
+    word_split(stdin);
+}
